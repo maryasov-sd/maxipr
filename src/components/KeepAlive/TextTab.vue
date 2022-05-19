ec2d3ceb021480772cb71b9be42fe48c11245b98
Maxim Palkin <max22012201@gmail.com>
2022-05-19 17:45:37 +0700

    сщб

---
+++ src/components/KeepAlive/TextTab.vue
@@ -0,0 +1,13 @@
+<template>
+<div>text</div>
+</template>
+
+<script>
+export default {
+  name: 'KeepAliveTextTab'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Keep.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+  <button
+    v-for="tab in tabs"
+    v-bind:key="tab"
+    v-bind:class="['tab-button', { active: currentTab === tab }]"
+    v-on:click="currentTab = tab"
+  >
+    {{ tab }}
+  </button>
+  <keep-alive>
+    <component :is="currentTabComponent" class="tab"></component>
+  </keep-alive>
+</div>
+</template>
+
+<script>
+
+import KeepAliveFormTab from '@/components/KeepAlive/FormTab'
+import KeepAliveTextTab from '@/components/KeepAlive/TextTab'
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Keep',
+  components: { KeepAliveTextTab, KeepAliveFormTab },
+  data () {
+    return {
+      currentTab: 'Form',
+      tabs: ['Form', 'Text']
+    }
+  },
+  computed: {
+    currentTabComponent () {
+      return 'KeepAlive' + this.currentTab + 'Tab'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
